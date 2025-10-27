32f306a695125a19d1e8e6e30331bbc98d405f20
Aashish <bharadwajaashish94@gmail.com>
2025-10-28 01:31:33 +0530

    Update prep-project.sh

--- prep-project.sh
+++ prep-project.sh
@@ -2,10 +2,15 @@ PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
 export serviceAccount=""$PROJECT_NUMBER"-compute@developer.gserviceaccount.com"
 
-bq mk neptune
-bq mk --schema message:STRING -t mars.rawmessages
 
-enable this cloudfunctions.googleapis.com
+
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable eventarc.googleapis.com
+
+bq mk neptune
+bq mk --schema message:STRING -t neptune.rawmessages
 
 gcloud pubsub topics create activities-topic --project=$GOOGLE_CLOUD_PROJECT
 
@@ -22,7 +27,15 @@ gcloud functions deploy processActivities \
     --project=playground-s-11-a324b2b9 \
     --region=us-central1
 
+    gcloud functions deploy processActivities \     
+           --runtime python310 \    
+           --trigger-topic activities-topic \    
+           --entry-point pubsub_to_bq   \   
+           --project=$GOOGLE_CLOUD_PROJECT   \  
+           --region=us-central1 \
+           --source .
 
+# gcloud pubsub topics publish activities-topic --message="new_account: test user" -- to manually push a message
 
 # deploying cloud function
 # gcloud functions deploy pubsub_to_bq \
